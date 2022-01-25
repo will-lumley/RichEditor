c23b7041228fd36ed60011c015a8c50a6fae121f
Will Lumley <will@lumley.io>
2022-01-25 15:19:57 +1100

    Added macColorPicker dependency.

--- Package.swift
+++ Package.swift
@@ -13,12 +13,13 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
+        .package(url: "https://github.com/will-lumley/macColorPicker.git", from: "1.2.3")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "RichEditor",
-            dependencies: [])
+            dependencies: ["macColorPicker"])
     ]
 )
